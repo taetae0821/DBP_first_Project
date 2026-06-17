b74fab7755d4d8db29f991ae6910e04a05117696
taeyu <s2530@e-mirim.hs.kr>
2026-06-17 10:37:08 +0900

    회원가입페이지 서버 연결

--- server.js
+++ server.js
@@ -38,26 +38,58 @@ app.post('/user', (req, res) => {
     console.log(req.body);
 
     const sql = `
-        INSERT INTO MEMBER
-        (name, email, phone, birth_date, join_date)
-        VALUES (?, ?, ?, ?, NOW())
-    `;
+    INSERT INTO MEMBER
+    (name, email, password, phone, birth_date, join_date)
+    VALUES (?, ?, ?, ?, ?, NOW())
+`;
 
     connection.query(
         sql,
         [name, email, phone, birth],
         (err, result) => {
             if (err) {
+
+                if (err.code === 'ER_DUP_ENTRY') {
+                return res.send('이미 가입된 이메일입니다.');
+        }
+
                 console.error(err);
                 return res.status(500).send('DB 저장 실패');
             }
-
-            console.log('DB 저장 성공');
-            res.send('DB 저장 성공');
+            if(email.indexOf("gmail.com") || email.indexOf("naver.com") || email.indexOf("daum.net") || email.indexOf("@")){
+                 console.log('DB 저장 성공');
+            res.redirect('/login.html');
+            }else{
+                res.send('이메일 형식이 잘못되었습니다');
+            }
         }
     );
 });
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
+    connection.query(
+        'SELECT * FROM MEMBER WHERE email = ?',
+        [email],
+        (err, results) => {
+            if (err) {
+                return res.status(500).send('DB 오류');
+            }
+
+            if (results.length === 0) {
+                return res.status(401).send('존재하지 않는 이메일입니다.');
+            }
+
+            const user = results[0];
+
+            if (user.password !== password) {
+                return res.status(401).send('비밀번호가 틀렸습니다.');
+            }
+            console.log("로그인 성공!");
+            res.redirect('/main.html');
+        }
+    );
+});
 
 app.listen(3000);
